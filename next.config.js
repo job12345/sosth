5a04436c6ec6a0612ed96506d13d3f410d53480c
job12345 <info@amitybroker.com>
2025-03-25 03:57:02 +0700

    fix: simplified next.config.js, removed PWA for debugging

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  scope: '/',
-  sw: 'sw.js',
-  skipWaiting: true
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
